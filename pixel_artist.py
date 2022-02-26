f26f86b6584e8cf3e824520dedf75959fb4ab95b
Igorexy <iak7@bk.ru>
2022-02-26 14:45:24 +0300

    pixels algorithm beta

--- pixel_artist.py
+++ pixel_artist.py
@@ -1,25 +1,12 @@
 # Ковертирование изображения в пиксельное
 # Изображение и изображениеЧтение изображений
 from PIL import Image
-import os
-from matplotlib import pyplot as plot
-from skimage import io, transform
+from skimage import io
 import numpy as np
-import statistics as stat
+import colors
 
 # чтение с файла
-#img_file1 = Image.open('smile_ball.png')
-img_file2 = io.imread('Gerb.jpg')
-# Размер данных после прочтения картинки:
-#print ("the picture's size: ", img_file1.size)
-#print ("the picture's shape: ", img_file2.shape)
-
-# Получить пиксели:
-#print(img_file1.getpixel((500,500)), img_file2[500][500])
-#print(img_file2[10][10])
-
-#img1 = Image.new("RGB", (100, 100))
-#img1.show()
+img_file2 = io.imread('smile_ball.png')
 
 
 imageSize = img_file2.shape     # получение размерности в виде: [x, y, z], где z=4 это кол-во сторон что ли, я не понял
@@ -30,28 +17,63 @@ for i in range(imageSize[0]):
     for j in range(imageSize[1]):
         pixels_list[i].append(tuple(img_file2[j][i]))
 
-# преобразование квадрата пикселей(16 на 16 в момент написания), в пиксели одного яркого цвета, цвет должен браться из базы данных цветов colors.py
+# преобразование квадрата пикселей(n на n), в пиксели среднего значения цвета RGB
+# причем цвета пикселей в формате RGB
+n=8     # корень из кол-ва пикселей
 pixels_list_edit = pixels_list
 try:
-    for i in range(0, imageSize[0], 4):
-        for j in range(0, imageSize[1], 4):
+    for i in range(0, imageSize[0], n):
+        j=0
+        while j<imageSize[1]:
             temp = []   #промежуточные пиксели
-            for m in range(4):
-                temp.append(pixels_list[i+m][j])
-                temp.append(pixels_list[i+m][j+1])
-                temp.append(pixels_list[i+m][j+2])
-                temp.append(pixels_list[i+m][j+3])
-            pixels = []
-            for k in range(4):
-                sumTemp = 0
-                for l in range(16):      # кол-во пикселей всего
-                    sumTemp += temp[l][k]
-                pixels.append(sumTemp//4)
-            pixels_list_edit[i][j] = pixels_list_edit[i][j+1] = pixels_list_edit[i+1][j] = pixels_list_edit[i+1][j+1] = tuple(pixels)
+            for k in range(n):
+                for l in range(n):
+                    temp.append(pixels_list[i+k][j+l])  # Добавление промежуточных пикселей для расчета среднего значения
+            # расчет среднего значения из кортежа цвета (r, g, b), отдельно средний по r, g и b
+            red = 0; green = 0; blue = 0
+            for k in range(len(temp)):
+                red += temp[k][0]
+                green += temp[k][1]
+                blue += temp[k][2]
+            red /= len(temp); green /= len(temp); blue /= len(temp)     # значения, очевидно, float, цвета же должны быть в int, далее преобразование
 
+            for k in range(n):
+                for l in range(n):
+                    pixels_list_edit[i+k][j+l] = (int(red), int(green), int(blue))
+            j+=n
 except:
     print('pixel broken')
 
+
+def rgb2hex(RGB):
+    """RGB - RHB tuple"""
+    return "#{:02x}{:02x}{:02x}".format(RGB[0], RGB[1], RGB[2])
+
+def hex2rgb(hexcode):
+    return tuple([int(hexcode[i:i+2], 16) for i in range(1, 7, 2)])
+
+def hex2int(hexcode):
+    return int(hexcode.replace('#', ''), 16)
+
+# Преобразование средних значений к ближайшему цвету из базы данных
+color_database = list(colors.hex_colors.values())
+number_of_colors = len(color_database)
+int_color_database = []
+for i in range(number_of_colors):
+    int_color_database.append(hex2int(color_database[i]))
+for i in range(imageSize[0]):
+    for j in range(imageSize[1]):
+        current_pixel = rgb2hex(pixels_list_edit[i][j])  # текущий пиксель изображения в формате hex
+        current_pixel = hex2int(current_pixel)
+        color_index = 0    # индекс цвета, который должен заменить пиксель
+        proximity = abs(int_color_database[0] - current_pixel)    # целочисленное приближение к цвету из библиотеки
+        #расчет приближения
+        for k in range(1, number_of_colors):
+            if proximity > abs(int_color_database[k] - current_pixel):
+                proximity = abs(int_color_database[k] - current_pixel)
+                color_index = k
+        pixels_list_edit[i][j] = hex2rgb(color_database[color_index])
+print(proximity)
 
 
 # преобразование из матрицы в список, для использования в функции putdata()
--- test.py
+++ test.py
@@ -1,6 +1,51 @@
+from unicodedata import decimal
 import numpy as np
 import statistics as stat
+import math
 
-temp = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12],  [13, 14, 15, 16]] 
+temp = [(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12),  (13, 14, 15, 16)] 
 
-print( (1, 2) + (3, 4) )
+
+temp[1] = (65, 3, 1)
+
+
+
+
+
+
+
+print("all oke")
+#print(type(rgb2hex(0, 128, 64)))
+#print(hex2rgb(hex()))
+
+
+x = hex(16)
+#print(list(x[1:]).decode('hex'))
+
+
+def rgb2hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+def hex2fgb(hexcode):
+    return tuple([int(hexcode[i:i+2], 16) for i in range(1, 7, 2)])
+
+
+#sps2=[math.sin(i*math.pi/5+2) for i in range(100)]
+#print(sps2)
+
+
+#print(hex2fgb('#f32fab'))
+#print(rgb2hex(243, 47, 171))
+
+#print(int('f32fab', 16))
+
+def hex2int(hexcode):
+    return int(hexcode.replace('#', ''), 16)
+import colors
+color_database = list(colors.hex_colors.values())
+number_of_colors = len(color_database)
+int_color_database = []
+for i in range(number_of_colors):
+    int_color_database.append(hex2int(color_database[i]))
+
+print(int_color_database)
